0cf5f61656af36c5df488c21662133a6394eadec
Jo Chuang <josephch405@gmail.com>
2017-03-06 02:01:21 -0500

    Reworking workflow for modularity, added team info aggregation

--- Jo/logistic.py
+++ Jo/logistic.py
@@ -1,49 +1,21 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
-import matplotlib.pyplot as plt
-from sklearn.utils import shuffle
-#from sklearn.model_selection import GridSearchCV
 
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-data_dir = '../input/'
-df_results_orig = pd.read_csv(data_dir + 'RegularSeasonDetailedResults.csv')
-df_results = df_results_orig[df_results_orig['Season'] > 2010]
-df_resultsw = df_results[["Wfgm", "Wast", "Wdr", "Wstl", "Wto"]]
-df_resultsw = df_resultsw.rename(index = str, columns = {"Wfgm":"fgm","Wast":"ast","Wdr":"dr","Wstl":"stl", "Wto":"to"})
-df_resultsw["result"] = 1
-
-df_resultsl = df_results[["Lfgm","Last", "Ldr", "Lstl", "Lto"]]
-df_resultsl = df_resultsl.rename(index = str, columns = {"Lfgm":"fgm","Last":"ast","Ldr":"dr","Lstl":"stl", "Lto":"to"})
-df_resultsl["result"] = 0
-
-df_resultswl = pd.concat([df_resultsw, df_resultsl])
-df_resultswl = df_resultswl.sample(frac=1)
-df_resultswl.head()
-
-#x = df_resultswl['fgm'].values.reshape(-1, 1)
-#y = df_resultswl['result'].values.tolist()
-
-logR = LogisticRegression()
-logR.fit(df_resultswl.drop(['result'], axis=1), df_resultswl['result'])
-'''
-plt.figure(1, figsize=(4, 3))
-plt.clf()
-plt.scatter(x.ravel(), y, color='black', zorder=20)
-
-X_test = np.linspace(0, 300, 300)
-
-print(logR.intercept_)
-
-def model(x):
-    return 1 / (1 + np.exp(-x))
-loss = model(X_test * logR.coef_[0][0] + logR.intercept_[0]).ravel()
-plt.plot(X_test, loss, color='red', linewidth=3)
-'''
-logR.predict_proba([[30,10, 20, 10, 2]])
+def derive_model(df_results):
+    df_resultsw = df_results[["Wfgm", "Wast", "Wdr", "Wstl", "Wto"]]
+    
+    df_resultsw = df_resultsw.rename(index = str, columns = {"Wfgm":"fgm","Wast":"ast","Wdr":"dr","Wstl":"stl", "Wto":"to"})
+    df_resultsw["result"] = 1
+    
+    df_resultsl = df_results[["Lfgm","Last", "Ldr", "Lstl", "Lto"]]
+    df_resultsl = df_resultsl.rename(index = str, columns = {"Lfgm":"fgm","Last":"ast","Ldr":"dr","Lstl":"stl", "Lto":"to"})
+    df_resultsl["result"] = 0
+    
+    df_resultswl = pd.concat([df_resultsw, df_resultsl])
+    df_resultswl = df_resultswl.sample(frac=1)
+    
+    logR = LogisticRegression()
+    logR = logR.fit(df_resultswl.drop(['result'], axis=1), df_resultswl['result'])
+    
+    return logR
--- Jo/tree.py
+++ Jo/tree.py
@@ -1,32 +1,20 @@
 import numpy as np
 import pandas as pd
 from sklearn import tree
-import matplotlib.pyplot as plt
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-data_dir = '../input/'
-df_results_orig = pd.read_csv(data_dir + 'RegularSeasonDetailedResults.csv')
-df_results = df_results_orig[df_results_orig['Season'] > 2010]
-df_results = df_results[["Wfgm", "Wast", "Wdr", "Wstl", "Wto", "Lfgm","Last", "Ldr", "Lstl", "Lto"]]
-df_resultsw = df_results.rename(index = str, columns = {"Wfgm":"fgm1","Wast":"ast1","Wdr":"dr1","Wstl":"stl1", "Wto":"to1", "Lfgm":"fgm2","Last":"ast2","Ldr":"dr2","Lstl":"stl2", "Lto":"to2"})
-df_resultsw["result"] = 1
-
-df_resultsl = df_results.rename(index = str, columns = {"Wfgm":"fgm2","Wast":"ast2","Wdr":"dr2","Wstl":"stl2", "Wto":"to2", "Lfgm":"fgm1","Last":"ast1","Ldr":"dr1","Lstl":"stl1", "Lto":"to1"})
-
-df_resultsl["result"] = 0
-
-df_resultswl = pd.concat([df_resultsw, df_resultsl])
-df_resultswl = df_resultswl.sample(frac=1)
-df_resultswl.head()
-
-
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(df_resultswl.drop(['result'], axis=1), df_resultswl['result'])
-
-df_resultswl.drop(['result'], axis=1).sample(n = 1)
-
-list = [10,12,10,10,30,20,10,10,10,10]
-
-clf.predict_proba([list])
+def derive_model(df_results):
+    df_results = df_results[["Wfgm", "Wast", "Wdr", "Wstl", "Wto", "Lfgm","Last", "Ldr", "Lstl", "Lto"]]
+    df_resultsw = df_results.rename(index = str, columns = {"Wfgm":"fgm1","Wast":"ast1","Wdr":"dr1","Wstl":"stl1", "Wto":"to1", "Lfgm":"fgm2","Last":"ast2","Ldr":"dr2","Lstl":"stl2", "Lto":"to2"})
+    df_resultsw["result"] = 1
+    
+    df_resultsl = df_results.rename(index = str, columns = {"Wfgm":"fgm2","Wast":"ast2","Wdr":"dr2","Wstl":"stl2", "Wto":"to2", "Lfgm":"fgm1","Last":"ast1","Ldr":"dr1","Lstl":"stl1", "Lto":"to1"})
+    
+    df_resultsl["result"] = 0
+    
+    df_resultswl = pd.concat([df_resultsw, df_resultsl])
+    df_resultswl = df_resultswl.sample(frac=1)
+    
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(df_resultswl.drop(['result'], axis=1), df_resultswl['result'])
+    
+    return clf
