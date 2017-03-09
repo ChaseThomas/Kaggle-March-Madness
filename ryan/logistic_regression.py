6e8e1f10dfc3d43467f67b1055ca7a8e623db0ec
Ryan Butler <thebutlah@gmail.com>
2017-03-09 00:25:39 -0500

    Finished model
    ! After changing values of seed, I realized that I was probably
      artificially inflating the results, because the accuracies began
      to vary significantly

--- ryan/logistic_regression.py
+++ ryan/logistic_regression.py
@@ -1,3 +1,5 @@
+import os
+
 import tensorflow as tf
 import random
 from time import strftime
@@ -7,7 +9,7 @@ from time import time
 class LogisticRegression:
 
     def __init__(self, x_train, y_train, num_epochs=10000, beta=0.01, seed=None, load_model=None):
-        self.__seed = random.randint(0, 2**31) if seed is None else seed
+        self.__seed = random.randint(0, 2**30) if seed is None else seed
         self.__beta = beta
         self.__graph = tf.Graph()
         num_features = x_train.shape[1]
@@ -15,33 +17,32 @@ class LogisticRegression:
             # SETUP THE MODEL
             # Symbolic Vars
             self.__X = tf.placeholder(tf.float32, (None, num_features), name="Features")
-            self.__Y = tf.placeholder(tf.float32, (None, 1), name="Targets")
+            self.__Y = tf.placeholder(tf.int32, (None,), name="Targets")
 
             # Parameters
-            w = tf.Variable(tf.random_normal((num_features, 1), stddev=0.1, dtype=tf.float32, seed=self.__seed, name="Parameters"))
+            w = tf.Variable(tf.random_normal((num_features, 2), stddev=0.1, dtype=tf.float32, seed=self.__seed, name="Parameters"))
             b = tf.Variable(tf.random_normal((1,), stddev=0.1, dtype=tf.float32, seed=self.__seed, name="Bias"))
 
             # Output Function (before sigmoid, passed to the loss function)
-            y = tf.add(tf.matmul(self.__X, w), b, name="Y")
+            y = tf.add(tf.matmul(self.__X, w), b)
 
             # Output Function (after sigmoid, represents the actual predicted probabilities)
-            self.__y_hat = tf.sigmoid(y, name="Y-hat")
+            self.__y_hat = tf.nn.softmax(y, name="Y-hat")
 
             # Cost and Loss functions
             regularizer = tf.nn.l2_loss(w, name="Regularizer")  # Penalize parameters on their L2 norm squared
-            loss = tf.reduce_sum(
-                tf.nn.sigmoid_cross_entropy_with_logits(labels=self.__Y, logits=y),  # use cross entropy as loss
+            loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.__Y, logits=y),  # use cross entropy as loss
                 name="Loss"
             )
             cost = tf.add(self.__beta * regularizer, loss, name="Cost")  # The final cost function to minimize
 
             # Optimizer setup
-            train_step = tf.train.AdamOptimizer().minimize(cost)
-
+            train_step = tf.train.AdamOptimizer(0.0005).minimize(cost)
             # Summary data
-            match_results = tf.equal(tf.round(self.__y_hat), tf.round(self.__Y))  # Vector of bool representing success of predictions
+            match_results = tf.equal(tf.cast(tf.argmax(self.__y_hat, 1), dtype=tf.int32), self.__Y)  # Vector of bool representing success of predictions
             self.__accuracy = tf.reduce_mean(
-                tf.cast(match_results, tf.float32),  # scalar of percentage of correct predictions
+                tf.cast(match_results, dtype=tf.float32),  # scalar of percentage of correct predictions
                 name="Accuracy"
             )
             # Setup TensorFlow Session and initialize graph variables
@@ -56,27 +57,20 @@ class LogisticRegression:
                 else:
                     self.sess.run(tf.global_variables_initializer())
 
-                if x_train is None and y_train is None:
-                    print("Skipping training.")
-                elif x_train is not None and y_train is not None:
+                # Training loop for parameter tuning
+                print("Starting training for %d epochs" % num_epochs)
+                last_time = time()
+                for epoch in range(num_epochs):
+                    _, cost_val = self.sess.run([train_step, cost], feed_dict={self.__X: x_train, self.__Y: y_train})
+                    current_time = time()
+                    if (current_time-last_time) >= 5:
+                        last_time = current_time
+                        print("Current Cost Value: %.10f, Percent Complete: %f" % (cost_val, epoch/num_epochs))
+                print("Completed Training.")
 
-                    # Training loop for parameter tuning
-                    print("Starting training for %d epochs" % num_epochs)
-                    last_time = time()
-                    for epoch in range(num_epochs):
-                        _, cost_val = self.sess.run([train_step, cost], feed_dict={self.__X: x_train, self.__Y: y_train})
-                        current_time = time()
-                        if (current_time-last_time) >= 5:
-                            last_time = current_time
-                            print("Current Cost Value: %f, Percent Complete: %f" % (cost_val, epoch/num_epochs))
-                    print("Completed Training.")
-
-                    # Training Summary
-                    training_accuracy = self.sess.run(self.__accuracy, feed_dict={self.__X: x_train, self.__Y: y_train})
-                    print("Training Accuracy: %f" % training_accuracy)
-
-                else:
-                    raise ValueError("One of x_train, y_train was None but the other had data. Both must be consistent")
+                # Training Summary
+                training_accuracy = self.sess.run(self.__accuracy, feed_dict={self.__X: x_train, self.__Y: y_train})
+                print("Training Accuracy: %f" % training_accuracy)
 
     def predict_values(self, x_data):
         with self.sess.as_default():
@@ -91,5 +85,6 @@ class LogisticRegression:
             print("Saving Model")
             if save_path is None:
                 save_path = "saved-networks/LogisticRegression-%s.ckpt" % strftime("%Y-%m-%d_%H-%M-%S")
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
             path = self.__saver.save(self.sess, save_path)
             print("Model successfully saved in file: %s" % path)
--- ryan/main.py
+++ ryan/main.py
@@ -2,28 +2,29 @@ from sklearn.model_selection import train_test_split
 from ryan.preprocess import load_tourney_results
 from ryan.preprocess import preprocess_team_avg
 from ryan.preprocess import dataframes_to_matricies
+from ryan.preprocess import load_toruney_seeds
 from ryan.preprocess import preprocess_massey
 from ryan.logistic_regression import LogisticRegression
 from ryan.decision_tree import *
 from sklearn import preprocessing
 
 
-SEED = 1337
+SEED = 31337
 
 
 def main():
     massey_ordinals_df = preprocess_massey()
     tourney_results_df = load_tourney_results()
+    tourney_seeds_df = load_toruney_seeds()
     team_avgs_df = preprocess_team_avg()
-    x_matrix, y_matrix = dataframes_to_matricies(team_avgs_df, massey_ordinals_df, tourney_results_df)
-
+    x_matrix, y_matrix = dataframes_to_matricies(team_avgs_df, massey_ordinals_df, tourney_seeds_df, tourney_results_df)
     # Split data into testing and training sets
     x_train, x_test, y_train, y_test = train_test_split(
-        x_matrix, y_matrix, test_size=0.2, random_state=SEED
+        x_matrix, y_matrix, test_size=0.3, random_state=SEED
     )
 
     print("Beginning Logistic Regression Demo")
-    regression = LogisticRegression(x_train, y_train, num_epochs=250000, beta=0.01, seed=SEED)
+    regression = LogisticRegression(x_train, y_train, num_epochs=500000, beta=0.01, seed=SEED)
     print("Test Accuracy: %f" % regression.test_accuracy(x_test, y_test))
 
     '''scaler = preprocessing.StandardScaler()
