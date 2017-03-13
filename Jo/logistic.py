b29dc0c0877985242cc5dd52be57a1aba8c943a2
Jo Chuang <josephch405@gmail.com>
2017-03-13 03:10:41 -0400

    not a very well done piece of work, but learned a lot anyway

--- Jo/logistic.py
+++ Jo/logistic.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from dask.dataframe.core import DataFrame
+from bokeh.charts.utils import df_from_json
 
 class LogReg():
     #def __init__(self):
@@ -13,41 +15,60 @@ class LogReg():
         team1_data = self.team_data.loc[team1, :]
         team2_data = self.team_data.loc[team2, :]
         
-        print team1_data
+        team1_data = team1_data.reset_index()
+        team2_data = team2_data.reset_index()
         
-        pa = self.model.predict_proba(team1_data)[:, 1]
-        pb = self.model.predict_proba(team2_data)[:, 1]
+        bob = team1_data - team2_data
+        bob = bob.drop(axis = 1, labels = "team")
         
-        return self.log5(pa, pb)
+        pbob = self.model.predict_proba(bob)[:, 1]
+        
+        return pbob
     
     def get_prediction_int(self, team1, team2):
         team1_data = self.team_data.loc[team1, :]
         team2_data = self.team_data.loc[team2, :]
         
-        pa = self.model.predict_proba(team1_data)[:, 1]
-        pb = self.model.predict_proba(team2_data)[:, 1]
+        team1_data = team1_data.reset_index()
+        team2_data = team2_data.reset_index()
         
-        probs = self.log5(pa, pb)
+        bob = team1_data - team2_data
+        bob = bob.drop(axis = 1, labels = "team")
         
-        probs = probs.round(0)
+        pbob = self.model.predict_proba(bob)[:, 1]
         
-        return probs
+        pbob = pbob.round(0)
+        
+        return pbob
      
     def setup_model(self, df_results, team_data):
+        
         df_resultsw = df_results[["Wfgm", "Wast", "Wdr", "Wstl", "Wto"]]
-        
+        #df_resultsw = df_resultsw.reindex()
         df_resultsw = df_resultsw.rename(index = str, columns = {"Wfgm":"fgm","Wast":"ast","Wdr":"dr","Wstl":"stl", "Wto":"to"})
-        df_resultsw["result"] = 1
-        
         df_resultsl = df_results[["Lfgm","Last", "Ldr", "Lstl", "Lto"]]
+        #df_resultsl = df_resultsl.reindex()
         df_resultsl = df_resultsl.rename(index = str, columns = {"Lfgm":"fgm","Last":"ast","Ldr":"dr","Lstl":"stl", "Lto":"to"})
-        df_resultsl["result"] = 0
         
-        df_resultswl = pd.concat([df_resultsw, df_resultsl])
-        df_resultswl = df_resultswl.sample(frac=1)
+        df_wins = df_resultsw - df_resultsl
+        df_wins["result"] = 1
+        df_losses = df_resultsl - df_resultsw
+        df_losses["result"] = 0
+        
+        df_diffs = pd.concat([df_wins, df_losses])
+        df_diffs = df_diffs.sample(frac=1)
+        
+        #df_resultsw["result"] = 1
+        
+        #df_resultsl["result"] = 0
+        
+        #df_resultswl = pd.concat([df_resultsw, df_resultsl])
+        #df_resultswl = df_resultswl.sample(frac=1)
+        
+        print(df_diffs)
         
         logR = LogisticRegression()
-        logR = logR.fit(df_resultswl.drop(['result'], axis=1), df_resultswl['result'])
+        logR = logR.fit(df_diffs.drop(['result'], axis=1), df_diffs['result'])
         
         self.model = logR
         self.team_data = team_data
--- Jo/main.py
+++ Jo/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from logistic import LogReg
 from tree import DeciTree
+from itertools import product
 
 data_dir = '~/Desktop/MM2017/'
 
@@ -24,26 +25,51 @@ def pickRandomTrainingSet(df_results):
 
 def main():
     reg_details_orig = pd.read_csv(data_dir + 'RegularSeasonDetailedResults.csv')
+    tourney_hist = pd.read_csv(data_dir + 'TourneyDetailedResults.csv')
+    tourney_2017 = pd.read_csv(data_dir + 'TourneySeeds.csv')
     
     team_data = reg_details_orig[reg_details_orig['Season'] > 2015]
     team_aggregate = teamProcessing(team_data)
+    print(team_aggregate)
     
-    df_results = reg_details_orig[reg_details_orig['Season'] > 2010]
+    df_results = reg_details_orig[reg_details_orig['Season'] > 2015]
     
     logModel = LogReg()
     logModel.setup_model(df_results, team_aggregate)
     
+    tourney_hist['actual'] = 0
+    tourney_hist.loc[tourney_hist['Wteam'] < tourney_hist['Lteam'], ['actual']] = 1
+    should_swap = tourney_hist['Wteam'] > tourney_hist['Lteam']
+    tourney_hist.loc[should_swap, ['Wteam', 'Lteam']] = tourney_hist.loc[should_swap, ['Lteam', 'Wteam']].values
+    tourney_hist = tourney_hist.rename(columns={'Wteam':'team1',"Lteam":'team2'})
+    
+    #print(tourney_hist)
+    
+   # print(tourney_hist["team1"].tolist())
+    
     #get_prediction(array of team1s, array of team2s)
-    print logModel.get_prediction([1101, 1181], [1181, 1102])
-    print logModel.get_prediction_int([1101, 1181], [1181, 1102])
+    #print logModel.get_prediction(tourney_hist["team1"].tolist(), tourney_hist["team2"].tolist())
+    #pred_results = logModel.get_prediction(tourney_hist["team1"].tolist(), tourney_hist["team2"].tolist())
     
-    treeModel = DeciTree()
-    treeModel.setup_model(df_results, team_aggregate)
-    print treeModel.get_prediction([1101, 1181], [1181, 1102])
-    print treeModel.get_prediction_int([1101, 1181], [1181, 1102])
+    #tourney_hist['pred'] = pred_results
     
+    #print(tourney_hist)
     
+    #tourney_hist.to_csv(data_dir + "HistoricalPredictions.csv")
+    
+    #tourney_hist['pred'] = logModel.get_prediction_int(tourney_hist["team1"].tolist(), tourney_hist["team2"].tolist())
+    
+    #print(tourney_hist[tourney_hist.actual == tourney_hist.pred].sum())
     
     #treeModel = tree.derive_model(df_results)
+    
+    #tourney_results
+    teamList = tourney_2017["Team"].tolist()
+    tourney_pairings = pd.DataFrame(list(product(teamList, teamList)), columns=["team1", "team2"])
+    keep = tourney_pairings['team1'] < tourney_pairings['team2']
+    tourney_pairings = tourney_pairings.loc[keep,:]
+    tourney_pairings["pred"] = logModel.get_prediction(tourney_pairings["team1"].tolist(), tourney_pairings["team2"].tolist())
+    print(tourney_pairings)
+    tourney_pairings.to_csv(data_dir + "FinalPredictions.csv")
 
 main()
