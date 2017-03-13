25ddf0e8c1dc26d5db1216c19ff8aec8a9edb38e
Amit Mizrahi <amit@amizrahi.com>
2017-03-13 15:10:25 -0400

    Clean up combine csvs file

--- ensemble/combine_csvs.py
+++ ensemble/combine_csvs.py
@@ -2,21 +2,25 @@ import csv
 import glob
 import pandas
 
-def combine_csvs(directory):
-    running_df = None
-    for filename in glob.glob("{}/*.csv".format(directory)):
-        df = pandas.DataFrame.from_csv(filename, index_col=False)
-        df = df[sorted(df.columns)]
-        if running_df is None:
-            running_df = df
-        else:
-            running_df = running_df.append(df.copy())
-    running_df.to_csv("{}/combined_{}.csv".format(directory, directory))
+"""
+Helper functions for combining CSV output files from individual models.
+"""
 
-def combine_train_test():
-    print "Combining training csvs..."
-    combine_csvs("train")
-    print "Combining testing csvs..."
-    combine_csvs("test")
+def combine_csvs(directories):
+    """
+    Combines CSVs with the same column names in a given list of directories.
+    Outputs one merged CSV per directory, with columns in each file
+    sorted lexicographically by name.
+    """
+    for directory in directories:
+        running_df = None
+        for filename in glob.glob("{}/*.csv".format(directory)):
+            df = pandas.DataFrame.from_csv(filename, index_col=False)
+            df = df[sorted(df.columns)]
+            if running_df is None:
+                running_df = df
+            else:
+                running_df = running_df.append(df.copy())
+        running_df.to_csv("{}/combined_{}.csv".format(directory, directory))
 
-combine_train_test()
+combine_csvs(["train", "test"])
---
+++ ensemble/main.py
@@ -0,0 +1,6 @@
+from combine_csvs import combine_train_test
+from model import run_ensemble
+
+def main():
+    combine_train_test()
+    run_ensemble()
